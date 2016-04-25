d8ab422a801417c1fcdcf0cdd9d505111c16f138
Bradley Bossard <bradleybossard@gmail.com>
2016-04-25 17:38:01 +0000

    Fixes to using index.html as template, remove hot-module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: ['./src/index'], // .js after index is optional
@@ -10,11 +11,16 @@ module.exports = {
   },
   devServer: {
     inline: true,
-    hot: true,
-    contentBase: '.',
+    //hot: true,
+    contentBase: './dist',
     host: '0.0.0.0',
     port: 8000
   },  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
   module: {
     loaders: [
       { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
